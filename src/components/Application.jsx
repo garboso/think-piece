9ecce7c62bab1a1f124c42f237e7bc7a032b1d6f
christopher augusto <christopher@cromaqui.com>
2022-01-05 20:15:33 -0300

    refactor using context

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,41 +1,35 @@
 import React, { Component } from 'react';
-import { firestore, auth } from '../firebase';
-import { collectIdAndDocs } from '../utilities';
+import { auth, createUserProfileDocument } from '../firebase';
 import Posts from './Posts';
 import Authentication from './Authentication';
 
 class Application extends Component {
   state = {
-    posts: [],
     user: null
   };
 
-  unsubscribeFromFirestore = null;
   unsubscribeFromAuth = null;
 
   componentDidMount = async () => {
-    this.unsubscribeFromFirestore = firestore.collection('posts').onSnapshot(snapshot => {
-      const posts = snapshot.docs.map(collectIdAndDocs);
-      this.setState({ posts });
-    });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      const user = await createUserProfileDocument(userAuth);
 
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
       this.setState({ user });
     })
   };
 
   componentWillUnmount = () => {
-    this.unsubscribeFromFirestore();
+    this.unsubscribeFromAuth();
   };
 
   render() {
-    const { posts, user } = this.state;
+    const { user } = this.state;
 
     return (
       <main className="Application">
         <h1>Think Piece</h1>
         <Authentication user={user} />
-        <Posts posts={posts} />
+        <Posts />
       </main>
     );
   }
