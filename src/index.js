f1788ef7517532701e9486e42eb549c852ccf5f9
christopher augusto <christopher@cromaqui.com>
2022-01-05 21:02:38 -0300

    refactor user code using context

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import './index.scss';
 
 import Application from './components/Application';
 import PostsProvider from './providers/PostsProvider';
+import UserProvider from './providers/UserProvider';
 
 render(
-	<PostsProvider>
-			<Application />
-	</PostsProvider>,
+	<UserProvider>
+		<PostsProvider>
+				<Application />
+		</PostsProvider>
+	</UserProvider>,
 	document.getElementById('root'));
